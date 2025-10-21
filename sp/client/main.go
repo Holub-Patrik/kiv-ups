a34c6521655e5bd704fafcb39c85a50e1866595c
Holub Patrik <23bulohp@gmail.com>
2025-10-21 12:49:01 +0200

    a

--- sp/client/main.go
+++ sp/client/main.go
@@ -3,16 +3,18 @@ package main
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
+	// rl.SetConfigFlags(rl.FlagWindowResizable)
 	rl.InitWindow(800, 450, "raylib [core] example - basic window")
 	defer rl.CloseWindow()
 
-	rl.SetTargetFPS(60)
+	// rl.SetTargetFPS(60)
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 
-		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+		rl.ClearBackground(rl.Black)
+		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.White)
+		rl.DrawFPS(0, 0)
 
 		rl.EndDrawing()
 	}
